7b0074ef3d20bf7dd1246bce2e2f0e752842117e
Sho <sho84.kitzxo@gmail.com>
2024-12-26 23:25:17 +0900

    createアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: 'home#index'
 
-  resource :profile, only: [:show, :new]
+  resource :profile, only: [:show, :new, :create]
 end
