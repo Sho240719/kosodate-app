8b7e77be9c99a222df64954f182fc2d4740cf8cd
Sho <sho84.kitzxo@gmail.com>
2025-01-02 22:39:48 +0900

    jQueryを導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import $ from 'jquery';
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("jQuery is working!");
+
+  $('h1').on('click', () => {
+    window.alert('Hello!');
+  })
+});
