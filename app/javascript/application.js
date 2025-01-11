902a251c91ff1d3fbe396fdb241ac5a9e9a8d191
Sho <sho84.kitzxo@gmail.com>
2025-01-11 15:01:27 +0900

    いいねされた数を表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import initializeComments from "./modules/comment";
-import initializeLikes from "./modules/like";
+import '@hotwired/turbo-rails'
+import 'controllers'
+import initializeComments from './modules/comment';
+import initializeLikes from './modules/like';
+import initializePosts from './modules/post';
 
 initializeComments();
 initializeLikes();
+initializePosts();
