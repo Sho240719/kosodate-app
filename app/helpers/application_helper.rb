95e3173ba70eafc30c8f681dc13fe3b6b35837ce
Sho <sho84.kitzxo@gmail.com>
2025-01-12 15:32:15 +0900

    ヘッダーにリンクを設定 ポスト一覧ページのアクションを削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def login_logout_link
     if user_signed_in?
-      link_to "ログアウト", '#', class: "header-nav-link"
+      link_to "ログアウト", destroy_user_session_path, data: {turbo_method: :delete, turbo_confirm: 'ログアウトしますか？'}, class: "header-nav-link"
     else
-      link_to 'ログイン', '#', class: 'header-nav-link'
+      link_to 'ログイン', new_user_session_path, class: 'header-nav-link'
     end
   end
 end
