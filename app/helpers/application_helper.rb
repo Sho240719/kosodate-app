30b9b70b7ea068c2d443b154b8b0ea5edcaaeb66
Sho <sho84.kitzxo@gmail.com>
2025-01-12 14:21:08 +0900

    ログイン状態によってヘッダーの表示を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def login_logout_link
+    if user_signed_in?
+      link_to "ログアウト", '#', class: "header-nav-link"
+    else
+      link_to 'ログイン', '#', class: 'header-nav-link'
+    end
+  end
 end
