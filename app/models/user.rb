d282112f2b41d671a5eec903f5f33404784d7649
Sho <sho84.kitzxo@gmail.com>
2025-01-13 01:03:34 +0900

    フォローの関係を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
   has_one :profile, dependent: :destroy
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
 end
