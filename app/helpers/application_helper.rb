f602ceab654f8e2c96677024e9c13ff6531706d8
Sho <sho84.kitzxo@gmail.com>
2025-01-12 16:08:48 +0900

    ログイン時にすべてのナビゲーションリンクを表示する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-  def login_logout_link
+  def header_navigation_links
     if user_signed_in?
-      link_to "ログアウト", destroy_user_session_path, data: {turbo_method: :delete, turbo_confirm: 'ログアウトしますか？'}, class: "header-nav-link"
+      safe_join([
+        link_to("投稿一覧", posts_path, data: { turbo: false }, class: "header-nav-link"),
+        link_to("プロフィール", profile_path, class: "header-nav-link"),
+        link_to("ログアウト", destroy_user_session_path, data: {turbo_method: :delete, turbo_confirm: 'ログアウトしますか？'}, class: "header-nav-link")
+      ], "\n") # 各リンクを改行
     else
       link_to 'ログイン', new_user_session_path, class: 'header-nav-link'
     end
