3691b9fad60a5d2377981916f20a50331649c94e
Sho <sho84.kitzxo@gmail.com>
2025-01-17 17:49:30 +0900

    sidekiqの設定

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module KosodateApp
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    config.active_job.queue_adapter = :sidekiq
   end
 end
