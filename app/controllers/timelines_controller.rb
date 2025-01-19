ea8bc75cdde6804ede22058020aafc04b3a42ef9
Sho <sho84.kitzxo@gmail.com>
2025-01-19 14:42:26 +0900

    いいねした記事を表示、コードの共通化

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,5 +2,7 @@ class TimelinesController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    post_ids = current_user.likes.pluck(:post_id)
+    @posts = Post.where(id: post_ids).order(created_at: :desc)
   end
 end
