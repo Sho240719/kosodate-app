60e7250232fbc8cf750b6457a5d570cb9e3789c5
Sho <sho84.kitzxo@gmail.com>
2025-01-27 10:31:26 +0900

    リファクタリング

--- app/javascript/modules/post.js
+++ app/javascript/modules/post.js
@@ -1,21 +1,21 @@
 import $ from 'jquery';
 
 document.addEventListener('turbo:load', () => {
-  const $contentInput = $('.post-form .post-form-textarea'); // 入力エリア
-  const $charCount = $('.post-form #char-count'); // 文字数を表示するエリア
-  const maxLength = $contentInput.attr('maxlength'); // 入力可能な最大文字数を取得
+  const contentInput = $('.post-form-textarea'); // 文字を入力するエリア
+  const maxLength = contentInput.attr('maxlength');// 入力可能な最大文字数を取得
+  const charCount = $('#char-count'); // 文字数を表示するエリア
 
-  if ($contentInput.length && $charCount.length) {
-    // 入力イベントを監視
-    $contentInput.on('input', function () {
-      const currentLength = $(this).val().length; // 現在の文字数を取得
-      $charCount.text(`${currentLength} / ${maxLength}`); // 文字数を更新
+  if (contentInput.length && charCount.length) {
+    // 入力した文字数を表示
+    contentInput.on('input', function () {
+      const currentLength = $(this).val().length;
+      charCount.text(`${currentLength} / ${maxLength}`);
 
       // 文字数が上限を超えたら文字色を赤くする
       if (currentLength > maxLength) {
-        $charCount.css('color', 'red');
+        charCount.css('color', 'red');
       } else {
-        $charCount.css('color', 'black');
+        charCount.css('color', 'black');
       }
     });
   }
